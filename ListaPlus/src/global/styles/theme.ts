20c49f39174a8b55d61ed4ce58f19fba62c3c008
Miguel Bizzi <miguel.bizzi@aglets.com.br>
2023-05-15 13:10:59 -0300

    Estruturaçao do projeto, e adicionamento do styled components e typescript

---
+++ ListaPlus/src/global/styles/theme.ts
@@ -0,0 +1,41 @@
+export default {
+    colors: {
+        primary: '#019283',
+        primary_light: '#A4C1AF',
+
+        secondary: '#ff872c',
+        secondary_light: 'rgba(255, 135, 44, 0.5)',
+
+        success: '#12a454',
+        success_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#e83f5b',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        warning: '#F7BD2B',
+        warning_light: 'rgba(247,189,43, 0.5)',
+
+        information: '#03A9F4',
+        information_light: 'rgba(3,169,244, 0.5)',
+
+        gray: '#eaeaea',
+        gray_light: 'rgba(234,234,234, 0.5)',
+
+        green: '#658255',
+
+        shape: '#ffffff',
+
+        title: '#363f5f',
+        text: '#969cb2',
+        text_light: '#eeeeee',
+        text_dark: '#000000',
+
+        background: '#f0f2f5',
+    },
+    fonts: {
+        light: 'Roboto_300Light',
+        regular: 'Roboto_400Regular',
+        medium: 'Roboto_500Medium',
+        bold: 'Roboto_700Bold',
+    },
+}
---
+++ ListaPlus/src/screens/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+// import { Container } from './styles';
+
+const Login: React.FC = () => {
+    return (
+        <View>
+            <Text>aaaaaaaa</Text>
+        </View>
+    )
+}
+
+export default Login
